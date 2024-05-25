1ebd13f71488ba50ab83a98f3cde2071f39fd853
Ismael Nyambu <133232846+Isma3l-19@users.noreply.github.com>
2024-05-25 13:08:31 +0000

    added the comment in the script

--- youtube.py
+++ youtube.py
@@ -4,6 +4,6 @@ link = input("Enter the URL for the video you want to dowload: ")
 
 video = YouTube(link)
 
-stream = video.streams.get_highest_resolution()
+stream = video.streams.get_highest_resolution() #helps you download the highest resolution there is
 
 stream.download()
